d994ffe89e4cac326c7acca4b798b937b3f4248a
Daniel Schulz <faulolio@gmail.com>
2026-07-19 23:20:54 +0200

    docs: test

---
+++ docker-test.bat
@@ -0,0 +1,9 @@
+setlocal
+cd %~dp0
+call load-env
+for /f %%i in ('docker --context %DOCKER_CONTEXT% info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
+pushd compose-unity
+call docker run --rm -e UNITY_CREDENTIALS_USR -e UNITY_CREDENTIALS_PSW tmp/%DOCKER_IMAGE%:latest compose-unity exec unity-empty-project test && compose-unity exec unity-build test
+popd
+endlocal
+pause
