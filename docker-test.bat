727774927db4a8353074cf2f0e5fa9712d06f72e
Daniel Schulz <faulolio@gmail.com>
2026-07-24 16:27:11 +0200

    fix: load-env

--- docker-test.bat
+++ docker-test.bat
@@ -18,6 +18,11 @@ if not defined DOCKER_TEST_CMD (
     goto test_done
 )
 
+set "DOCKER_TEST_ARGS=%DOCKER_TEST_ARGS:~1,-1%"
+set "DOCKER_TEST_ARGS=%DOCKER_TEST_ARGS:\"="%"
+set "DOCKER_TEST_CMD=%DOCKER_TEST_CMD:~1,-1%"
+set "DOCKER_TEST_CMD=%DOCKER_TEST_CMD:\"="%"
+
 if defined DOCKER_OS (
     set "DOCKER_CONTEXT_ARGS=--context %DOCKER_OS%"
 ) else (
@@ -35,6 +40,9 @@ if not defined DOCKER_TEST_OS_ARGS (
     echo Missing DOCKER_TEST_ARGS_%DOCKER_OS% in .env
     goto test_done
 )
+
+set "DOCKER_TEST_OS_ARGS=%DOCKER_TEST_OS_ARGS:~1,-1%"
+set "DOCKER_TEST_OS_ARGS=%DOCKER_TEST_OS_ARGS:\"="%"
 
 docker %DOCKER_CONTEXT_ARGS% run --rm %DOCKER_TEST_ARGS% %DOCKER_TEST_OS_ARGS% tmp/%DOCKER_IMAGE%:latest %DOCKER_TEST_CMD%
 set "TEST_EXIT_CODE=%ERRORLEVEL%"
