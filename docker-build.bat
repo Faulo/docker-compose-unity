4dec04c679ef800236703e8aef32c2c6f68de919
Daniel Schulz <faulolio@gmail.com>
2026-07-24 16:22:31 +0200

    refactor: move project-specifics to env

--- docker-build.bat
+++ docker-build.bat
@@ -1,9 +1,38 @@
+@echo off
 setlocal
-cd %~dp0
-call load-env
-for /f %%i in ('docker --context %DOCKER_CONTEXT% info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-pushd compose-unity
-call docker --context %DOCKER_CONTEXT% build -t tmp/%DOCKER_IMAGE%:latest . -f Dockerfile.%DOCKER_OS%.dockerfile
+cd /d "%~dp0"
+call load-env.bat
+
+set "BUILD_EXIT_CODE=1"
+set "DOCKER_OS=%~1"
+set "DOCKER_CONTEXT_ARGS="
+
+if not defined DOCKER_IMAGE (
+    echo Missing DOCKER_IMAGE in .env
+    goto build_done
+)
+
+if defined DOCKER_OS (
+    set "DOCKER_CONTEXT_ARGS=--context %DOCKER_OS%"
+) else (
+    for /f %%i in ('docker info --format "{{.OSType}}"') do set "DOCKER_OS=%%i"
+)
+
+if not defined DOCKER_OS (
+    echo Failed to determine the Docker OS
+    goto build_done
+)
+
+if not exist "%DOCKER_IMAGE%\Dockerfile.%DOCKER_OS%.dockerfile" (
+    echo Missing Dockerfile: %DOCKER_IMAGE%\Dockerfile.%DOCKER_OS%.dockerfile
+    goto build_done
+)
+
+pushd "%DOCKER_IMAGE%"
+docker %DOCKER_CONTEXT_ARGS% build --tag tmp/%DOCKER_IMAGE%:latest --file Dockerfile.%DOCKER_OS%.dockerfile .
+set "BUILD_EXIT_CODE=%ERRORLEVEL%"
 popd
-endlocal
+
+:build_done
 pause
+endlocal & exit /b %BUILD_EXIT_CODE%
--- docker-test.bat
+++ docker-test.bat
@@ -1,9 +1,44 @@
+@echo off
 setlocal
-cd %~dp0
-call load-env
-for /f %%i in ('docker --context %DOCKER_CONTEXT% info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-pushd compose-unity
-call docker run --rm -e UNITY_CREDENTIALS_USR -e UNITY_CREDENTIALS_PSW -e EMAIL_CREDENTIALS_USR -e EMAIL_CREDENTIALS_PSW tmp/%DOCKER_IMAGE%:latest compose-unity exec unity-empty-project test && compose-unity exec unity-build test
-popd
-endlocal
+cd /d "%~dp0"
+call load-env.bat
+
+set "TEST_EXIT_CODE=1"
+set "DOCKER_OS=%~1"
+set "DOCKER_CONTEXT_ARGS="
+set "DOCKER_TEST_OS_ARGS="
+
+if not defined DOCKER_IMAGE (
+    echo Missing DOCKER_IMAGE in .env
+    goto test_done
+)
+
+if not defined DOCKER_TEST_CMD (
+    echo Missing DOCKER_TEST_CMD in .env
+    goto test_done
+)
+
+if defined DOCKER_OS (
+    set "DOCKER_CONTEXT_ARGS=--context %DOCKER_OS%"
+) else (
+    for /f %%i in ('docker info --format "{{.OSType}}"') do set "DOCKER_OS=%%i"
+)
+
+if not defined DOCKER_OS (
+    echo Failed to determine the Docker OS
+    goto test_done
+)
+
+call set "DOCKER_TEST_OS_ARGS=%%DOCKER_TEST_ARGS_%DOCKER_OS%%%"
+
+if not defined DOCKER_TEST_OS_ARGS (
+    echo Missing DOCKER_TEST_ARGS_%DOCKER_OS% in .env
+    goto test_done
+)
+
+docker %DOCKER_CONTEXT_ARGS% run --rm %DOCKER_TEST_ARGS% %DOCKER_TEST_OS_ARGS% tmp/%DOCKER_IMAGE%:latest %DOCKER_TEST_CMD%
+set "TEST_EXIT_CODE=%ERRORLEVEL%"
+
+:test_done
 pause
+endlocal & exit /b %TEST_EXIT_CODE%
